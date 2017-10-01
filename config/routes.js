2034fb3d892be4c0cd380bcda16bfd4dd0784b30
Guy Sopher <guyso@wix.com>
2017-10-01 19:37:57 +0300

    wall => blabs

--- config/routes.js
+++ config/routes.js
@@ -36,8 +36,8 @@ module.exports.routes = {
     view: 'homepage'
   },
 
-  '/wall': {
-    view: 'wall'
+  '/blabs': {
+    view: 'blabs'
   }
 
   /***************************************************************************
