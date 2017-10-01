9f681a40c00f67dd783eddfdab7488225d81fae8
Guy Sopher <guyso@wix.com>
2017-10-01 19:13:22 +0300

    remove es6 strings

--- assets/js/blabs.js
+++ assets/js/blabs.js
@@ -1,25 +1,25 @@
 function createBlab(blabData) {
-  var newBlab = $(`
-    <div id="${blabData.id}" class="blab-container">
-      <div class="blab-image">
-        <img src="https://email2pic.herokuapp.com/gravatar/${blabData.email}">
-      </div>
-      <div class="blab-body">
-        <div class="blab-email">${blabData.email}</div>
-        <div class="blab-content">${blabData.content}</div>
-        <div class="blab-actions">
-          <a href="#" class="delete-blab" onclick="deleteBlab('${blabData.id}')">delete</a>
-          <span> · </span>
-          <a href="#" class="upvote-blab" onclick="upvoteBlab('${blabData.id}')">upvote (<span class="blab-upvotes">${blabData.upvotes}</span>)</a>
-          <span> · </span>
-          <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=${encodeURI(document.location.host)}%2Fblabs%2F${blabData.id}&t=${blabData.content}">share</a>
-          <span> · </span>
-          <a>${blabData.createdAt.toLocaleString()}</a>
-        </div>
-      </div>
-    </div>
-  `);  
-  $('#blabs-container').prepend(newBlab);
+  var newBlab = $('' + 
+  '<div id="' + blabData.id + '" class="blab-container">' +
+    '<div class="blab-image">' +
+      '<img src="https://email2pic.herokuapp.com/gravatar/' + blabData.email + '">' +
+    '</div>' +
+    '<div class="blab-body">' +
+      '<div class="blab-email">' + blabData.email + '</div>' +
+      '<div class="blab-content">' + blabData.content + '</div>' +
+      '<div class="blab-actions">' +
+        '<a href="#" class="delete-blab" onclick="deleteBlab(\'' + blabData.id + '\')">delete</a>' +
+        '<span> · </span>' +
+        '<a href="#" class="upvote-blab" onclick="upvoteBlab(\'' + blabData.id + '\')">upvote (<span class="blab-upvotes">' + blabData.upvotes + '</span>)</a>' +
+        '<span> · </span>' +
+        '<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=' + encodeURI(document.location.host) + '%2Fblabs%2F' + blabData.id + '&t=' + blabData.content + '">share</a>' +
+        '<span> · </span>' +
+        '<a>' + blabData.createdAt.toLocaleString() + '</a>' +
+      '</div>' +
+    '</div>' +
+  '</div>' +
+'');  
+$('#blabs-container').prepend(newBlab);
 }
 
 function deleteBlab(blabId) {
