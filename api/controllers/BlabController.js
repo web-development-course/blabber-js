262753e4773a036c2f546f28acf1292e8971efca
Guy Sopher <guyso@wix.com>
2017-10-01 18:54:37 +0300

    add blab api

---
+++ api/controllers/BlabController.js
@@ -0,0 +1,11 @@
+/**
+ * BlabController
+ *
+ * @description :: Server-side logic for managing blabs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
